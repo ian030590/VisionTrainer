ae05358e50f8544dfa53b4130f30b2b52885a0ec
Ian0305 <rainbowh9490@gmail.com>
2026-06-16 16:56:26 +0800

    doc: change credit words

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -384,8 +384,8 @@ export const en: Record<TranslationKey, string> = {
   'credits.foveaflow.desc': 'Referenced MOT design from Jesper-N/foveaflow',
   'credits.gaborPatching.title': 'Gabor Patching',
   'credits.gaborPatching.desc': 'Referenced game mechanics from Fordi/gabor-patching',
-  'credits.visiontherapy.title': 'Vision Therapy Tools',
-  'credits.visiontherapy.desc': 'Referenced vision therapy training tools and exercises from visiontherapy.github.io',
+  'credits.visiontherapy.title': 'Hart Chart Training',
+  'credits.visiontherapy.desc': 'Referenced vision therapy training tools and exercises from visiontherapy/visiontherapy.github.io',
 
   // Extra
   'nav.brand': 'Vision Trainer',
